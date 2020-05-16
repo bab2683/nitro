08a4d2eaed5d41431837137172f481df50241507
bab2683 <barahona.braulio@gmail.com>
2020-05-16 15:51:03 +0100

    feature(editing): added editing functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -10,8 +10,7 @@ import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent, PostComponent],
-  imports: [AppRoutingModule, BrowserModule, HttpClientModule],
-  providers: [],
+  imports: [BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { ParsedPost } from '@models';
 
@@ -7,6 +8,35 @@ import { ParsedPost } from '@models';
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
   @Input() public post: ParsedPost;
+
+  public form: FormGroup;
+  public editing: boolean = false;
+  public submitted: boolean = false;
+  public editableFields: string[] = ['author', 'location'];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group(this.generateFields());
+  }
+
+  public onSubmit(): void {
+    this.submitted = true;
+    if (this.form.valid) {
+      this.editableFields.forEach((field) => {
+        this.post[field] = this.form.get(field).value;
+      });
+
+      this.submitted = false;
+    }
+  }
+
+  private generateFields() {
+    return this.editableFields.reduce((result, current) => {
+      result[current] = [this.post[current], Validators.required];
+      return result;
+    }, {});
+  }
 }
